6a02c69c289cde522c5657a4fd8e02ff1470ecb7
kanekoshinya <knksny777@icloud.com>
2021-05-12 03:47:33 +0900

    ユーザー新規登録機能完了

--- CustomerManagement/src/main/java/com/example/app/controller/UserController.java
+++ CustomerManagement/src/main/java/com/example/app/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.app.from.signup;
 import com.example.app.service.UserDetailsServiceImpl;
@@ -15,6 +17,9 @@ import com.example.app.service.UserDetailsServiceImpl;
 public class UserController {
 	@Autowired
 	UserDetailsServiceImpl UserDetailsServiceImpl;
+
+	@Autowired
+    PasswordEncoder passwordEncoder;
 
 	@GetMapping("/login")
 	public String login(){
@@ -28,11 +33,13 @@ public class UserController {
 	public String signup(){
 		return "signup";
 	}
-	@PostMapping("signups")
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(@ModelAttribute signup signup) {
 		// フォームの中から名前と年齢を取得してデータベース登録処理へ
-		UserDetailsServiceImpl.save(signup.getUsername(), signup.getPassword());
+		String encodedPassword = passwordEncoder.encode(signup.getPassword());
+		UserDetailsServiceImpl.save(signup.getUsername(), encodedPassword);
 		return "redirect:/";
 	}
+
 
 }

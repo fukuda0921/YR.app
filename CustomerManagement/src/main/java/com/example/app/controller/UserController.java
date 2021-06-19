7c2afca8c82fd150abbf66a79f862f004451ae34
kanekoshinya <knksny777@icloud.com>
2021-06-19 22:55:14 +0900

    見込みリスト一覧作成

--- CustomerManagement/src/main/java/com/example/app/controller/UserController.java
+++ CustomerManagement/src/main/java/com/example/app/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.example.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.app.entity.Client;
+import com.example.app.entity.User;
 import com.example.app.from.signup;
+import com.example.app.service.ClientService;
 import com.example.app.service.UserDetailsServiceImpl;
 
 
@@ -21,12 +29,24 @@ public class UserController {
 	@Autowired
     PasswordEncoder passwordEncoder;
 
+	@Autowired//オートワイヤリング設定(DIコンテナから型が一致するものを取り出しインジェクションする)
+	 private ClientService clientService;
+
 	@GetMapping("/login")
 	public String login(){
 		return "login";
 	}
+
 	@GetMapping("/")
-	public String index(){
+	public String index(Model model,@AuthenticationPrincipal User user){
+		List<Client> client = clientService.findAllClient();
+		List<Client> clients= new ArrayList<>();
+        for(int i=0;i < client.size();i++) {
+        	if(user.getId().equals(client.get(i).getUser_id())&&2 == client.get(i).getStatus()) {
+        		clients.add(client.get(i));
+        	}
+        }
+        model.addAttribute("client", clients);
 		return "top";
 	}
 
